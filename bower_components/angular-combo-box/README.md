92782ba2c02a9728555ea51a3c091e6239fa656d
Bradley Trager <bradley.trager@contractor.cengage.com>
2015-03-23 12:21:29 -0400

    add dependencies

---
+++ bower_components/angular-combo-box/README.md
@@ -0,0 +1,35 @@
+# angular-combo-box
+Lightweight Angular JS directive which combines a select element with a text input.
+
+[![Build Status](https://travis-ci.org/bradleytrager/angular-combo-box.svg)](https://travis-ci.org/bradleytrager/angular-combo-box)
+
+##Install
+
+####Manual download 
+Download latest release from [here](https://github.com/bradleytrager/angular-combo-box/releases)
+
+####Bower
+```sh
+bower install angular-combo-box
+```
+
+##Usage
+```html
+<combo-box
+	label="--Select--" //The default option if nothing is chosen
+    options="options" //An array of predefined options
+    combo-model="comboModel" //The bound value of the combo box
+    other-label="An option not on the list..." //Optional label for other input
+></combo-box>
+```
+```JavaScript
+//Include 'ngComboBox as a module dependency
+angular.module('ngComboBoxExample', ['ngComboBox'])
+    .controller('myController', function ($scope) {
+    $scope.options = [
+        //List of predefined options    
+    ];
+    //The bound value of the combo box
+    $scope.comboModel = '';
+});
+```
---
+++ bower_components/angular-combo-box/src/angular-combo-box.js
@@ -0,0 +1,52 @@
+angular.module('ngComboBox', [])
+    .directive('comboBox', function() {
+        return {
+            restrict: 'E',
+            scope: {
+                options: '=',
+                comboModel: '=',
+                otherLabel: '@?',
+                label: '@?'
+            },
+            template: '<select ng-model="selected" ng-change="optionChanged(selected)">' +
+                '    <option value="">{{label}}</option>' +
+                '    <option ng-repeat="option in options">{{option}}</option>' +
+                '    <option value="other" ng-bind="otherLabel"></option>' +
+                '</select>' +
+                '<br />' +
+                '<input type="text" ng-model="other" ng-change="optionChanged(selected)" ng-show="selected === \'other\'" />',
+            compile: function(el, attrs) {
+                if (!attrs.otherLabel) {
+                    attrs.otherLabel = 'Other';
+                }
+                if (!attrs.label) {
+                    attrs.label = '';
+                }
+            },
+            controller: 'comboBoxController'
+        };
+    })
+    .controller('comboBoxController', ['$scope', function($scope) {
+        $scope.optionChanged = setComboModel;
+        if ($scope.comboModel) {
+            setInput($scope.comboModel);
+        }
+
+        function setInput(value) {
+            if ($scope.options.indexOf(value) !== -1) {
+                $scope.selected = value;
+            } else {
+                $scope.selected = 'other';
+                $scope.other = value;
+            }
+        }
+
+        function setComboModel(option) {
+            if (option !== 'other') {
+                $scope.other = '';
+                $scope.comboModel = option;
+            } else {
+                $scope.comboModel = $scope.other;
+            }
+        }
+    }]);
