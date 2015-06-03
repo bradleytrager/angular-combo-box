47a749e6ff155c6b97459232c563f9f6b2222457
Bradley Trager <bradleytrager@Bradleys-MacBook-Pro.local>
2015-06-02 22:14:55 -0400

    fix build

--- Gruntfile.js
+++ Gruntfile.js
@@ -74,6 +74,9 @@ module.exports = function(grunt) {
         },
 
         coveralls: {
+            options: {
+                force: true
+            },
             src: {
                 src: 'coverage/**/lcov.info'
             },
